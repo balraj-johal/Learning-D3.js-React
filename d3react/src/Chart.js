c17df2e0b8e5b64f0bcea6dd9294aa553d312a81
Balraj Johal <balraj-johal@users.noreply.github.com>
2021-01-28 22:24:01 +0000

    tidy code

--- d3react/src/Chart.js
+++ d3react/src/Chart.js
@@ -3,14 +3,17 @@ import * as d3 from 'd3';
 
 export default function Chart(props) {
     let {data, height, width, color} = props;
-    const ref = React.useRef(null);
+
+    const ref = React.useRef(null); //creates ref for d3 to select with
     const group = d3.select(ref.current);
 
+    //calls function whenever variable provided in second parameter
+    //is changed
     useEffect(() => {
-        const update = group.selectAll('rect') //select all ref items
-            .data(data); //join test data
+        const update = group.selectAll('rect')
+            .data(data); //joins data
 
-        let bars = update.enter()
+        update.enter() //returns DOM items that need to be created (?)
             .append('rect') //create required rects
             .merge(update)
             .attr('x', (d, index) => index * 70) //translate x axis of rects
@@ -19,14 +22,13 @@ export default function Chart(props) {
             .attr('height', (d, index) => d * 10)
             .attr('fill', color);
 
-        update.exit().remove();
-    }, )
+        update.exit().remove(); //remove "redundant" rects
+    }, [props.data])
 
     return(
-        <svg width={width} height={height} style={
-            {backgroundColor:'grey', padding: '1em'}
-        }>
-          <g ref={ref} />
+        <svg width={width} height={height} 
+            style={{backgroundColor:'grey', padding: '1em'}}>
+            <g ref={ref} />
         </svg>
     )
 }
