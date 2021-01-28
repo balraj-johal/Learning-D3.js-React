1f6a7a583ce13174f2a4cab05cc16e5a4192e5b3
Balraj Johal <balraj-johal@users.noreply.github.com>
2021-01-28 21:06:23 +0000

    attempt to populate chart

--- d3react/src/App.js
+++ d3react/src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 import Chart from './Chart.js';
 
 function App() {
+  const testData = [100, 20, 3, 4, 2, 10, 6, 7, 10, 9];
+  const width = window.innerWidth - 30;
+  const height = 500;
+  const color = 'tomato';
+
   return (
     <div className="App">
-      <Chart></Chart>
+      <Chart data={testData} width={width} height={height} color={color}></Chart>
     </div>
   );
 }
--- d3react/src/Chart.js
+++ d3react/src/Chart.js
@@ -6,12 +6,26 @@ export default function Chart(props) {
 
     useEffect(() => {
         let refAccess = d3.select(ref.current);
-        refAccess.style('background-color', 'red');
-    })
+        refAccess.append('svg') //create svg item and append to div
+            .attr('width', props.width)
+            .attr('height', props.height)
+            .style('background-color', 'grey')
+            .style('padding', '1em')
+            .selectAll('rect') //select all ref items
+                .data(props.data) //join test data
+                //return all rects corresp. to data elements that aren't currently in DOM
+                .enter()
+                .append('rect') //create required rects
+                .attr('x', (data, index) => index * 70) //translate x axis of rects
+                .attr('y', (data, index) => props.height - 10 * data) //translate y axis of rects
+                .attr('width', 65)
+                .attr('height', (data, index) => data * 10)
+                .attr('fill', props.color)
+    }, [])
 
     return(
         <div ref={ref}>
-            Test
+            
         </div>
     )
 }
